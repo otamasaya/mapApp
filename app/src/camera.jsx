68b98a027cf2518350444e694df61f8f5f80e4e4
HandaRyo <kd1335461@st.kobedenshi.ac.jp>
2024-06-25 13:15:29 +0900

    pick Image button working

--- app/src/camera.jsx
+++ app/src/camera.jsx
@@ -1,13 +1,18 @@
 import { useCallback, useEffect, useState, useRef } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import { Stack, useFocusEffect } from "expo-router";
-
 import {
   useCameraPermission,
   useCameraDevice,
   Camera,
 } from "react-native-vision-camera";
-import { ActivityIndicator } from "react-native-web";
+import * as ImagePicker from "expo-image-picker";
 
 const CameraScreen = () => {
   const device = useCameraDevice("back", {
@@ -15,6 +20,7 @@ const CameraScreen = () => {
   });
   const { hasPermission, requestPermission } = useCameraPermission();
   const [isActive, setIsActive] = useState(false);
+  const [image, setImage] = useState("");
 
   const camera = useRef < Camera > null;
 
@@ -40,6 +46,15 @@ const CameraScreen = () => {
     console.log(photo);
   };
 
+  async function pickImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [9, 16],
+      quality: 1,
+    });
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <Stack.Screen options={{ headerShown: false }} />
@@ -62,6 +77,21 @@ const CameraScreen = () => {
           borderRadius: 75,
         }}
       />
+      <TouchableOpacity
+        //onPress={}
+        style={{
+          position: "absolute",
+          alignSelf: "center",
+          bottom: 50,
+          left: 40,
+          width: 45,
+          height: 45,
+          backgroundColor: "black",
+          borderRadius: 25,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></TouchableOpacity>
     </View>
   );
 };
