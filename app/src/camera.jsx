4abaa268da100140eb9cbbd3ea8a0506c2d793ee
HandaRyo <kd1335461@st.kobedenshi.ac.jp>
2024-06-25 09:51:16 +0900

    ボタンの途中

--- app/src/camera.jsx
+++ app/src/camera.jsx
@@ -7,6 +7,7 @@ import {
   useCameraDevice,
   Camera,
 } from "react-native-vision-camera";
+import { ActivityIndicator } from "react-native-web";
 
 const CameraScreen = () => {
   const device = useCameraDevice("back", {
@@ -15,10 +16,14 @@ const CameraScreen = () => {
   const { hasPermission, requestPermission } = useCameraPermission();
   const [isActive, setIsActive] = useState(false);
 
+  const camera = useRef < Camera > null;
+
   useFocusEffect(
     useCallback(() => {
+      console.log("on duty");
       setIsActive(true);
       return () => {
+        console.log("navigating away");
         setIsActive(false);
       };
     }, [])
@@ -30,15 +35,20 @@ const CameraScreen = () => {
     }
   }, [hasPermission]);
 
-  const onTakePicturePressed = () => {};
+  const onTakePicturePressed = async () => {
+    const photo = await camera.current?.takePhoto();
+    console.log(photo);
+  };
 
   return (
     <View style={{ flex: 1 }}>
       <Stack.Screen options={{ headerShown: false }} />
       <Camera
+        useRef={camera}
         style={StyleSheet.absoluteFill}
         device={device}
         isActive={isActive}
+        photo={true}
       />
       <Pressable
         onPress={onTakePicturePressed}
---
+++ app/src/image.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native-web";
+import React from "react";
+
+const Image = () => {
+  return (
+    <View>
+      <Text>Image</Text>
+    </View>
+  );
+};
