03e83bf6253eed0c4fc6f0bdd8148ab805f21806
HandaRyo <kd1335461@st.kobedenshi.ac.jp>
2024-06-26 14:04:20 +0900

    camera

--- app/src/camera.jsx
+++ app/src/camera.jsx
@@ -11,8 +11,12 @@ import {
   useCameraPermission,
   useCameraDevice,
   Camera,
+  PhotoFile,
+  image,
 } from "react-native-vision-camera";
 import * as ImagePicker from "expo-image-picker";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Button } from "react-native-web";
 
 export default function CameraScreen() {
   const cameraRef = useRef(null);
@@ -21,6 +25,8 @@ export default function CameraScreen() {
   const { hasPermission, requestPermission } = useCameraPermission();
   const [isActive, setIsActive] = useState(false);
   const [image, setImage] = useState("");
+  const [photo, setPhoto] = useState < PhotoFile > null;
+  const [flash, setFlash] = useState(false);
 
   useFocusEffect(
     useCallback(() => {
@@ -46,9 +52,20 @@ export default function CameraScreen() {
       }
       const photo = await cameraRef.current.takePhoto();
       console.log(photo);
+      setPhoto(photo);
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const uploadPhoto = async () => {
+    if (!photo) {
+      return;
+    }
+
+    const result = await fetch("file://${file.path}");
+    const data = await result.blob();
+    // console.log(data);
   };
 
   async function pickImage() {
@@ -63,41 +80,70 @@ export default function CameraScreen() {
   return (
     <View style={{ flex: 1 }}>
       <Stack.Screen options={{ headerShown: false }} />
+
       <Camera
         ref={cameraRef}
         style={StyleSheet.absoluteFill}
         device={device}
         photo={true}
         photoQualityBalance="quality"
-        isActive={isActive}
+        isActive={isActive && !photo}
       />
-      <Pressable
-        onPress={onTakePicturePressed}
-        style={{
-          position: "absolute",
-          alignSelf: "center",
-          bottom: 50,
-          width: 75,
-          height: 75,
-          backgroundColor: "white",
-          borderRadius: 75,
-        }}
-      ></Pressable>
-      <TouchableOpacity
-        onPress={pickImage}
-        style={{
-          position: "absolute",
-          alignSelf: "center",
-          bottom: 50,
-          left: 40,
-          width: 45,
-          height: 45,
-          backgroundColor: "black",
-          borderRadius: 25,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      ></TouchableOpacity>
+
+      {photo ? (
+        <>
+          <FontAwesome5
+            onPress={() => setPhoto(undefined)}
+            name="arrow-left"
+            size={25}
+            color="red"
+            style={{ position: "absolute", top: 50, left: 30 }}
+          />
+          <View
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              right: 0,
+              paddingBottom: 50,
+              backgroundColor: "rgba(0,0,0,0.40)",
+            }}
+          >
+            <Button title="Upload" onPress={uploadPhoto} />
+          </View>
+          <image source={{ uri: photo.path }} style={StyleSheet.absoluteFill} />
+        </>
+      ) : (
+        <>
+          <Pressable
+            onPress={onTakePicturePressed}
+            style={{
+              position: "absolute",
+              alignSelf: "center",
+              bottom: 50,
+              width: 75,
+              height: 75,
+              backgroundColor: "white",
+              borderRadius: 75,
+            }}
+          />
+          <TouchableOpacity
+            onPress={pickImage}
+            style={{
+              position: "absolute",
+              alignSelf: "center",
+              bottom: 50,
+              left: 40,
+              width: 45,
+              height: 45,
+              backgroundColor: "black",
+              borderRadius: 25,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          />
+        </>
+      )}
     </View>
   );
 }
