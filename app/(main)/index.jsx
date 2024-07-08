ae52ae72c2722e92d01a1425a82102df53a5583f
Misetsu <nakitamak@gmail.com>
2024-07-08 10:26:13 +0900

    fix import

--- app/(main)/index.jsx
+++ app/(main)/index.jsx
@@ -9,12 +9,15 @@ import {
   Dimensions,
   TouchableOpacity,
   StyleSheet,
+  ActivityIndicator,
 } from "react-native";
 import { Link } from "expo-router";
 import Geolocation from "@react-native-community/geolocation";
 import MapView, { Marker } from "react-native-maps";
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
 
-import customMapStyle from "../component/mapLayout";
+// import customMapStyle from "../component/mapLayout";
 
 const { width, height } = Dimensions.get("window"); //デバイスの幅と高さを取得する
 const ASPECT_RATIO = width / height; //アスペクト比
@@ -355,5 +358,248 @@ const MyModal = ({ visible, imageUri, onClose }) => {
     </Modal>
   );
 };
+
+const customMapStyle = [
+  {
+    featureType: "poi.business", // ビジネス（ビル、店舗など）のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business", // ビジネス（ビル、店舗など）のアイコンを非表示
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.attraction", // 観光スポットのラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.government", // 政府機関のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.medical", // 医療施設のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park", // 公園のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.place_of_worship", // 宗教施設のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.school", // 学校のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.sports_complex", // スポーツ施設のラベルを非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road", // 道路の号線表示を非表示
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality", // 町、村、区のラベルを非表示
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality", // 町、村、区のラベルのアウトラインを非表示
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.neighborhood", // 住所（丁目）のラベルを非表示
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.neighborhood", // 住所（丁目）のラベルのアウトラインを非表示
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+
+  //ここから地図の色
+  {
+    featureType: "landscape.natural", // 自然地形の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#66bb66",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made", //地面の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e0ffe0",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#6699ff", // 水の色を青色に変更
+      },
+    ],
+  },
+  {
+    featureType: "road", //  一般道の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#404040",
+      },
+    ],
+  },
+  {
+    featureType: "road", // 一般道の枠線
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#fcfcfc",
+        weight: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.highway", // 高速道路の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#808080",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway", // 高速道路の枠線の色
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#fcfcfc",
+        weight: 1,
+      },
+    ],
+  },
+  {
+    featureType: "poi.park", // 公園の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#99dd66",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line", // 鉄道の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#33ccff",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line", // 鉄道の枠線の太さ
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#ffffff",
+        weight: 1,
+      },
+    ],
+  },
+  {
+    featureType: "poi.school", // 教育機関の色
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ffeecc",
+      },
+    ],
+  },
+  {
+    // 医療機関の背景色を指定（例: 薄いピンク色）
+    featureType: "poi.medical",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ffdddd",
+      },
+    ],
+  },
+];
 
 export default TrackUserMapView;
